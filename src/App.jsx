22d9a7cb47a721eedf1e74752187445b794a3a72
Pedro <pdroariasvilela@gmail.com>
2023-11-08 16:40:42 -0500

    create showLogin function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
 import LoginForm from "./components/login-form"
 import SignUpForm from "./components/signup-form"
 
 function App(){
 
+     const [showLogin , setShowLogin] = useState(true)
+
+     const handleClick = () =>{
+          setShowLogin(!showLogin)
+     }
 
      return(
         
         <div>
-            <h1>Welcome to PokeCollection</h1>
-             {/* <LoginForm/> */}
-             <SignUpForm/>
+          <h1>Welcome to PokeCollection</h1>
+          {showLogin ? <LoginForm/> : <SignUpForm/> }
+          <button onClick={handleClick}>
+               {showLogin? "Create Account" : "Login"}
+          </button>
         </div>
      )
 }
