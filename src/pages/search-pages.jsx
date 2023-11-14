f4f3f7d8d1766b43532b73d1f837b12a2c29e433
Pedro <pdroariasvilela@gmail.com>
2023-11-14 18:28:52 -0500

    handleSearchSubmit && setState

--- src/pages/search-pages.jsx
+++ src/pages/search-pages.jsx
@@ -3,63 +3,117 @@ import { useState } from "react"
 import Input from "../components/input"
 import { getPokemon } from "../services/pokeapi-service"
 import styled from "@emotion/styled"
+import { RiStarFill } from "react-icons/ri";
 
- function SearchPage() {
 
-    const [query , setQuery] = useState('')
-
-    const [pokemon , setPokemon] = useState(null)
-
-    const PokemonImg = styled.img`
-        max-width : 150px;
+const PokemonImg = styled.img`
+    max-width : 150px;
     `
+const FavoriteButton = styled.button`
+    display: flex;
+    align-items : center;
+    justify-content: center;
+    gap: .5rem;
+    background-color : gray;
+    border : none ;
+    padding : .5rem 1rem;
+    font-weight : bold;
+    color: white;
+    cursor : pointer;
+`
 
 
-    function PokemonData({pokemon}){
+function SearchPage() {
+
+    const [query, setQuery] = useState('')
+
+    // const [pokemon, setPokemon] = useState(null)
+
+    // const [error , setError] = useState(null)
+
+    const [state , setState] = useState ({
+        status : 'iddle',
+        data : null,
+        error : null
+    })
+
+    const {status , data : pokemon , error} = state
+
+
+    function pokeId(id) {
+        id = String(id)
+
+        const newId = id.length < 2 ? `#00${id}` : id.length < 3 ? `#0${id}` : `#${id}`
+
+        return newId
+    }
+
+    function PokemonData({ pokemon }) {
 
         return (
 
             <div>
                 <h2>{pokemon.name}</h2>
-                <p>{pokemon.id}</p>
-                <PokemonImg src={pokemon.sprites.other["official-artwork"].front_default}/>
-                {pokemon.types.map((element)=>
-                <p key={element.slot}>{element.type.name}</p>
+                <p>{pokeId(pokemon.id)}</p>
+                <PokemonImg src={pokemon.sprites.other["official-artwork"].front_default} />
+                {pokemon.types.map((element) =>
+                    <p key={element.slot}>{element.type.name}</p>
                 )}
-                <p>Weight : {pokemon.weight /10} kg</p>
-                <p>Height : {pokemon.height/10} m</p>
-                
-                <button>Mark as favorite</button>
+                <p>Weight : {pokemon.weight / 10} kg</p>
+                <p>Height : {pokemon.height / 10} m</p>
+
+                <FavoriteButton>
+                    <RiStarFill />
+                    Mark as favorite
+                </FavoriteButton>
             </div>
 
         )
     }
 
 
-     function handleSearchSubmit(event){
+    function handleSearchSubmit(event) {
 
         event.preventDefault();
-        
+
         console.log(query)
 
-        getPokemon(query).then((data) => setPokemon(data)).catch((error)=> console.log(error))
+        getPokemon(query).
+        then( (data)=> {
+            setState({
+                status : 'succes',
+                data : data,
+                error : null
+            })
+        })
+
+            .catch((_error) => {
+                setState({
+                    status : 'error',
+                    data : null ,
+                    error : "No se encontró resultados"
+                })
+            })
     }
 
-    
+
 
     return (
         <div>
             <h1>searchPage</h1>
             <form onSubmit={handleSearchSubmit}>
-                <Input 
+                <Input
                     name='query'
                     placeholder='Pokemon name'
                     value={query}
                     onChange={(event) => setQuery(event.target.value)}
-                    />
+                />
                 <button>Search</button>
             </form>
-            {pokemon? <PokemonData pokemon={pokemon} /> : 'No data pokemon'}
+            { status === 'iddle'  && 'No data pokemon' }
+            { status === 'succes' && <PokemonData pokemon={pokemon} />}
+            { status === 'error' && <p style={{color : "red"}}>{error}</p>}
+            
         </div>
     )
 }
